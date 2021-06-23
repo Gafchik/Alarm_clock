3bd9263078284ad5642b6cc3adb547aab1c94ef0
Dice_kl17 <Dice_kl17@student.itstep.org>
2021-06-23 16:02:57 +0300

    Добавьте файлы проекта.

---
+++ WpfApp3/ModelView/ModelView.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
+namespace WpfApp3
+{
+    internal class ModelView : INotifyPropertyChanged
+    {
+        Timer t; 
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged; // ивент обновления
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+           => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        #endregion
+
+        public ModelView()
+        {
+            t = new Timer();
+            InitializeComponen();
+            t.Interval = 1000;
+            t.Elapsed += T_Elapsed;
+            t.Start();
+        }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e) => InitializeComponen();
+
+
+
+        public void InitializeComponen()
+        {
+            Delimiter = ":";
+            TimeH = DateTime.Now.Hour.ToString();
+            TimeM = DateTime.Now.Minute.ToString();
+            DataD = DateTime.Now.Day.ToString();
+            DataM = DateTime.Now.Month.ToString();
+            DataY = DateTime.Now.Year.ToString();
+        }
+
+        private string delimiter; // разделитель
+
+        public string Delimiter
+        {
+            get { return delimiter; }
+            set { delimiter = value; OnPropertyChanged("Delimiter"); }
+        }
+
+        private string timeH; // Часы
+
+        public string TimeH
+        {
+            get { return timeH; }
+            set { timeH = value; OnPropertyChanged("TimeH"); }
+        }
+
+
+        private string timeM; // МИнуты
+
+        public string TimeM
+        {
+            get { return timeM; }
+            set { timeM = value; OnPropertyChanged("TimeM"); }
+        }
+        private string dataD; // Дни
+
+        public string DataD
+        {
+            get { return dataD; }
+            set { dataD = value; OnPropertyChanged("DataD"); }
+        }
+
+        private string dataM; // месяца
+
+        public string DataM
+        {
+            get { return dataM; }
+            set { dataM = value; OnPropertyChanged("DataM"); }
+        }
+
+        private string dataY; // год
+
+        public string DataY
+        {
+            get { return dataY; }
+            set { dataY = value; OnPropertyChanged("DataY"); }
+        }
+
+
+
+
+
+       
+    }
+}
---
+++ WpfApp3/View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp3.View;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> them;
+        public MainWindow()
+        {
+            them = new List<string>() { "DayStyle", "NightStyle" };
+
+            InitializeComponent();
+            DataContext = new ModelView();
+            Application.Current.Resources.Clear();
+            ResourceDictionary resource = (ResourceDictionary)Application.LoadComponent(new Uri("Style/" + them[0] + ".xaml", UriKind.Relative));
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) => new Setting().ShowDialog();
+
+
+    }
+}
